4f04068c79a61af0a9c1a8e70dddcf53d4174469
xboris <boris.tikhonov.21@gmail.com>
2024-07-07 15:56:26 +0300

    created models Tag & Task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
